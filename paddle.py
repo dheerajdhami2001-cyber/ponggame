9743cc8464445b7e9ffd065bfc6e482c37c1cc65
Dheeraj Dhami <dheerajdhami@Dheerajs-MacBook-Air.local>
2025-09-19 14:01:41 +0530

    Initial commit

---
+++ paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle,Screen
+
+screen = Screen
+class Paddle(Turtle):
+    def __init__(self,position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.speed("fastest")
+        self.penup()
+        self.goto(position)
+
+    def move_up(self):
+        self.goto(self.xcor(), self.ycor() + 20)
+
+    def move_down(self):
+        self.goto(self.xcor(), self.ycor() - 20)
+
